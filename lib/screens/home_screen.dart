566e8368f3f50c3869c09c3e1455a29afb090a89
nokibul82 <nokibul82@gmail.com>
2024-01-28 21:14:32 +0600

    popup menu inplemented

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -12,8 +13,7 @@ class _HomeScreenState extends State<HomeScreen>
   late TabController _tabController;
   @override
   void initState() {
-    _tabController =
-        TabController(length: 4, vsync: this, initialIndex: 0);
+    _tabController = TabController(length: 4, vsync: this, initialIndex: 0);
     super.initState();
   }
 
@@ -23,21 +23,68 @@ class _HomeScreenState extends State<HomeScreen>
       appBar: AppBar(
         title: const Text("Whatsapp"),
         actions: [
+          IconButton(
+              onPressed: () {}, icon: const Icon(Icons.camera_alt_outlined)),
           IconButton(onPressed: () {}, icon: const Icon(Icons.search_rounded)),
-          IconButton(onPressed: () {}, icon: const Icon(Icons.more_vert)),
+          PopupMenuButton(
+            offset: const Offset(0, 50),
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+            onSelected: (value) => print("$value selected"),
+            itemBuilder: (context) {
+              return const [
+                PopupMenuItem(
+                  value: "New group",
+                  child: Text(
+                    "New group",
+                  ),
+                ),
+                PopupMenuItem(
+                  value: "New broadcast",
+                  child: Text("New broadcast"),
+                ),
+                PopupMenuItem(
+                  value: "Linked devices",
+                  child: Text("Linked devices"),
+                ),
+                PopupMenuItem(
+                  value: "Starred messages",
+                  child: Text("Starred messages"),
+                ),
+                PopupMenuItem(
+                  value: "Settings",
+                  child: Text("Settings"),
+                ),
+              ];
+            },
+          ),
         ],
         bottom: TabBar(
-          indicatorColor: Theme.of(context).secondaryHeaderColor,
-          controller: _tabController,
+            indicatorColor: Theme.of(context).secondaryHeaderColor,
+            controller: _tabController,
             tabs: const [
-          Tab(
-            icon: Icon(Icons.camera_alt_rounded),
-          ),
-          Tab(text: "Chat"),
-          Tab(text: "Status"),
-          Tab(text: "Calls"),
-        ]),
+              Tab(
+                icon: Icon(CupertinoIcons.person_3_fill),
+              ),
+              Tab(text: "Chats"),
+              Tab(text: "Status"),
+              Tab(text: "Calls"),
+            ]),
       ),
+      body: TabBarView(controller: _tabController, children: [
+        Container(
+          color: Colors.redAccent,
+        ),
+        Container(
+          color: Colors.blueAccent,
+        ),
+        Container(
+          color: Colors.greenAccent,
+        ),
+        Container(
+          color: Colors.redAccent,
+        ),
+      ]),
     );
   }
 }
